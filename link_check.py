1efbe6a79c40da7b02c22e955cd2a35821509cdc
Chris Jones <christopherjones72521@gmail.com>
2020-11-06 16:51:11 -0500

    I've added a link checking for 200 status.

--- link_check.py
+++ link_check.py
@@ -8,33 +8,41 @@ to_visit = [] # links not vistited in the horizon
 visited = [] # links already visited. Can be used to reference against to avoid duplicate checking
 internal_links = [] # anything without https:// these are the links we will be checking.
 external_links = [] # bucket to dump external links. May be useful for reporting
+bad_links = [] # anything that does not return a 200
 
 page = requests.get(url)
 
 soup = BeautifulSoup(page.text, 'html.parser')
 
 # Find all links
-for link in soup.find_all('a'): # Should I pop each link found into this list?
+for link in soup.find_all('a'):
     horizon.append(link.get('href'))
 
-# clean up data in this list (ex. '/', '', '#', 'javascript:;', begins with '#', begins with 'http')
-# iterage through the horizon list and move internal links to the internal link list
-for link in horizon:
+# Scrub list of all irrelevant links
+for link in horizon: # there has to be a better way to do the below
     if link != None and link != '/' and link != '' and link != '#' and link[0:10] != 'javascript' and link[0] != '#' and link[0:4] != 'http' and link[0:6] != 'mailto':
         internal_links.append(link)
     elif link != None and len(link) > 4 and link[0:4] == 'http':
         external_links.append(link)
 
+#TODO begin checking each page with a console message explaining which page we're gathering or checking links on
+#TODO dedup the list of urls 
+
 for link in internal_links:
-    print(link) 
+    full_url = 'https://www.nyrr.org' + link
+    page = requests.get(full_url)
+    print('Status: ' + str(page.status_code) + ' | ' + link)
+    # if full_url.status_code != '200':
+    #     bad_links.append(link)
 
+# for link in bad_links: 
+#     print(bad_links)
 
-# Create function to check all links on first page for status 200
-# for link in horizon: 
-#   link.status_code # if status code != 200 then store this link in a list
-
-# Create function to visit all links on first page of links and grab links from these pages
+#TODO Include website URL as arguments to script
+#TODO Create function to visit all links on first page of links and grab links from these pages.
 # will have to be a recursive function which automatically visits discovered links and continues until no non-duplicate links exist
-# Check each link against a list of links. If link does not exist in list, add it to the to_visit list
-# Add conditional check for 'is internal' and create separate list 'external_links' for any that don't qualify
 
+#TODO Check each link against a list of links. If link does not exist in list, add it to the to_visit list
+# The approach will be to create the master list of links to check before checking
+
+#TODO Add conditional check for 'is internal' and create separate list 'external_links' for any that don't qualify
