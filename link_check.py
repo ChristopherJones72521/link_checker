5a06298b99cd4f7f253e8b218f2c04673e1f25b8
Chris Jones <christopherjones72521@gmail.com>
2020-11-10 18:12:38 -0500

    I'll never solve this javascript:; issue :(

--- link_check.py
+++ link_check.py
@@ -1,4 +1,7 @@
 import requests
+import validators
+from logzero import logger
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
 #TODO Add User Agent data
@@ -13,52 +16,65 @@ bad_links = [] # anything that does not return a 200
 
 def crawl_website(root_link):
     tmp_horizon = []
-    # print('Currently checking: %s', root_link)
     
-    if root_link:
-        if check_if_new_link(root_link):            
-            #Check to see if this link returns a 200 then set it to a variable
-            page = requests.get(root_link)
-            print(root_link + ' is a new link returning a ' + str(page.status_code))
-            visited.append(root_link)
-            # print('These are the to_visit links: %s ', str(to_visit))
-            # print('These are the visited links: %s ', str(visited))
-            if page.status_code == 200:
-                soup = BeautifulSoup(page.text, 'html.parser')
+    if validators.url(root_link):
+        if root_link:
+            if check_if_new_link(root_link):            
+                #Check to see if this link returns a 200 then set it to a variable
+                try:                    
+                    page = requests.get(root_link)
+                    logger.info(root_link + ' is a new link returning a ' + str(page.status_code))
+                except Exception as e:
+                    logger.exception('to_visit:' + str(to_visit))
+                    logger.exception('root_link' + root_link)
+                    logger.exception(e)
 
-                # Find all links
-                for link in soup.find_all('a'):                    
-                    tmp_horizon.append(link.get('href'))                    
+                # Add current link to visted links list
+                visited.append(root_link)
 
-                # Scrub list of all irrelevant links
-                for link in tmp_horizon: # there has to be a better way to do the below
-                    if link != None and link != '/' and link != '' and link[0:10] != 'link.aspx?' and link != '~' and link != '#' and link[0:10] != 'javascript' and link[0] != '#' and link[0:4] != 'http' and link[0:6] != 'mailto':
-                        internal_links.append('https://www.nyrr.org' + link)
-                    elif link != None and len(link) > 4 and link[0:4] == 'http':
-                        external_links.append(link)
+                # If the site returns a success code, then scrape for links
+                if page.status_code == 200:
+                    soup = BeautifulSoup(page.text, 'html.parser')
 
-                # Check all links in internal links to see if they've been checked before
-                for tmp_link in internal_links:
-                    # print('checking: ' + tmp_link)
-                    if check_if_new_link(tmp_link):                        
-                        print('Found new link: %s adding to to_visit list', tmp_link)
-                        to_visit.append(tmp_link)
+                    # Find all links
+                    for link in soup.find_all('a'):                    
+                        tmp_horizon.append(link.get('href'))                    
+
+                    # Scrub list of all irrelevant links
+                    for link in tmp_horizon: # there has to be a better way to do the below   
+                        try:         
+                            if link != None and link != '/' and link != '' and link[0:10] != 'link.aspx?' and link[0:3] != 'tel' and link != '/~' and link != '#' and link[0:10] != 'javascript' and link[0] != '#' and link[0:4] != 'http' and link[0:6] != 'mailto':
+                                # logger.info('The value of link is: ' + link)
+                                full_url = urljoin('https://www.nyrr.org', link)
+                                # logger.info('The full URL being appended to internal links is: ' + full_url)
+                                internal_links.append(full_url)
+                            elif link != None and len(link) > 4 and link[0:4] == 'http':
+                                external_links.append(link)
+                        except Exception as e:
+                            logger.exception(e)
+
+                    # Check all links in internal links to see if they've been checked before
+                    for tmp_link in internal_links:
+                        # logger.info('checking: ' + tmp_link)
+                        if check_if_new_link(tmp_link):                        
+                            # logger.info('Found new link: %s adding to to_visit list', tmp_link)
+                            to_visit.append(tmp_link)
+                else:
+                    logger.info('This page is not online') # Create a list for pages that are offline. 
+                    bad_links.append(root_link)
             else:
-                print('This page is not online') # Create a list for pages that are offline. 
-                bad_links.append(root_link)
+                logger.info('There are no new links to check')
         else:
-            print('There are no new links to check')
+            logger.info('There are no links to check')
     else:
-        print('There are no links to check')
-    # print(to_visit)
-    crawl_website(to_visit.pop(0))
+        logger.info('Not a valid URL')
+    if to_visit:
+        crawl_website(to_visit.pop(0))
+    else: 
+        logger.info("You've reached the end of the list. Cheers!")
 
 def check_if_new_link(link_to_check):
-    # if link_to_check:
-    # I need to check if the link exists in either list
     if link_to_check not in to_visit and link_to_check not in visited:
-        # print(link_to_check + ' is not in the to_visit list: ' + str(link_to_check not in to_visit))
-        # print(link_to_check + ' is not in the visited list: ' + str(link_to_check not in visited))
         to_visit.append(link_to_check)
         return True
     else: 
