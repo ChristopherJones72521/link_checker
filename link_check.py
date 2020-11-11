466deea673bfcec77a0e3b7e9c14c7b6ef0e203f
Chris Jones <christopherjones72521@gmail.com>
2020-11-10 23:58:23 -0500

    The most functional version yet! But seeing an issue with 'RecursionError: maximum recursion depth exceeded during compilation'

--- link_check.py
+++ link_check.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 #TODO Add User Agent data
 
-url = 'https://www.nyrr.org/'
+url = 'https://www.nyrr.org' # This should be the hostname / root URL as we will be appending to it later. 
 horizon = [] # All links on the site across all pages. Will be useful for indexing. 
 to_visit = [] # links not vistited in the horizon
 visited = [] # links already visited. Can be used to reference against to avoid duplicate checking
@@ -26,46 +26,46 @@ def crawl_website(root_link):
                 try:                    
                     page = requests.get(root_link)
                     logger.info(root_link + ' is a new link returning a ' + str(page.status_code))
+
+                    # Add current link to visted links list
+                    visited.append(root_link)
+
+                    # If the site returns a success code, then scrape for links
+                    if page.status_code == 200 and page.headers['Content-Type'] == 'text/html; charset=utf-8':
+                        soup = BeautifulSoup(page.text, 'html.parser')
+
+                        # Find all links
+                        for link in soup.find_all('a'):                    
+                            tmp_horizon.append(link.get('href'))                    
+
+                        # Scrub list of all irrelevant links
+                        for link in tmp_horizon: # there has to be a better way to do the below   
+                            try:         
+                                if link != None and link != '/' and link != '' and link[0:10] != 'link.aspx?' and link[0:3] != 'tel' and link != '/~' and link != '#' and link[0:10] != 'javascript' and link[0] != '#' and link[0:4] != 'http' and link[0:6] != 'mailto':
+                                    # logger.info('The value of link is: ' + link)
+                                    full_url = urljoin(url, link) 
+                                    # logger.info('The full URL being appended to internal links is: ' + full_url)
+                                    internal_links.append(full_url)
+                                elif link != None and len(link) > 4 and link[0:4] == 'http':
+                                    external_links.append(link)
+                            except Exception as e:
+                                logger.exception(e)
+
+                        # Check all links in internal links to see if they've been checked before
+                        for tmp_link in internal_links:
+                            # logger.info('checking: ' + tmp_link)
+                            if check_if_new_link(tmp_link):                        
+                                # logger.info('Found new link: %s adding to to_visit list', tmp_link)
+                                to_visit.append(tmp_link)
+                    else:
+                        logger.info(root_link + ' is not online ' + '| Status code: ' + str(page.status_code)) # Create a list for pages that are offline. 
+                        bad_links.append(root_link + " | Status code: " + str(page.status_code))
                 except Exception as e:                    
                     logger.exception('root_link: ' + root_link)
                     pprint.pprint((to_visit))
                     logger.exception(e)
                     bad_links.append(root_link)
                     pass
-
-                # Add current link to visted links list
-                visited.append(root_link)
-
-                # If the site returns a success code, then scrape for links
-                if page.status_code == 200:
-                    soup = BeautifulSoup(page.text, 'html.parser')
-
-                    # Find all links
-                    for link in soup.find_all('a'):                    
-                        tmp_horizon.append(link.get('href'))                    
-
-                    # Scrub list of all irrelevant links
-                    for link in tmp_horizon: # there has to be a better way to do the below   
-                        try:         
-                            if link != None and link != '/' and link != '' and link[0:10] != 'link.aspx?' and link[0:3] != 'tel' and link != '/~' and link != '#' and link[0:10] != 'javascript' and link[0] != '#' and link[0:4] != 'http' and link[0:6] != 'mailto':
-                                # logger.info('The value of link is: ' + link)
-                                full_url = urljoin('https://www.nyrr.org', link)
-                                # logger.info('The full URL being appended to internal links is: ' + full_url)
-                                internal_links.append(full_url)
-                            elif link != None and len(link) > 4 and link[0:4] == 'http':
-                                external_links.append(link)
-                        except Exception as e:
-                            logger.exception(e)
-
-                    # Check all links in internal links to see if they've been checked before
-                    for tmp_link in internal_links:
-                        # logger.info('checking: ' + tmp_link)
-                        if check_if_new_link(tmp_link):                        
-                            # logger.info('Found new link: %s adding to to_visit list', tmp_link)
-                            to_visit.append(tmp_link)
-                else:
-                    logger.info(root_link + ' is not online ' + '| Status code: ' + str(page.status_code)) # Create a list for pages that are offline. 
-                    bad_links.append(root_link + " | Status code: " + str(page.status_code))
     #         else:
     #             logger.info('There are no new links to check')
     #     else:
